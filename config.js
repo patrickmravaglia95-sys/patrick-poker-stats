aec36d00be1dc258412fa0d2d3a52e23e7d2260b
patrickmravaglia95-sys <patrickmravaglia95@gmail.com>
2026-08-06 07:11:00 -0300

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Depois vamos preencher estes dois valores com os dados do seu projeto Supabase.
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_SUPABASE";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_DO_SEU_SUPABASE";
+const SUPABASE_URL = "https://pxfpolnkuejqbpsutfgs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable__kdshE2U56KZkDyobt8Rqw_z_9R-WxS";
